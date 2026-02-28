1cfb7de59acfd99305a1597a2d3cdbbfeb1e5c68
mawerb <maorbarzilay8@gmail.com>
2026-02-27 19:00:30 -0800

    initial commit

---
+++ filecopy.c
@@ -0,0 +1,22 @@
+/**
+ * filecopy.c
+ * 
+ * This program copies files using a pipe.
+ *
+ */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define READ_END 0
+#define WRITE_END 1
+
+int main(int argc, char *argv[])
+{
+	/* 
+	Your code is here
+	*/
+}
