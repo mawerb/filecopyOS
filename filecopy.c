3170b02f99143ee75f63d181d97acdb70f4a0051
mawerb <maorbarzilay8@gmail.com>
2026-02-28 22:11:46 -0800

    Added error handling

--- filecopy.c
+++ filecopy.c
@@ -7,6 +7,7 @@
 
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -33,6 +34,10 @@ int main(int argc, char *argv[])
 	}
 
 	int pid = fork();
+	if (pid == -1) {
+		printf("An error occured while creating the child process.\n");
+		exit(1);
+	}
 	if (pid == 0) {
 		close(fd[WRITE_END]);
 
@@ -40,16 +45,28 @@ int main(int argc, char *argv[])
 		ssize_t n;
 		int dst_fd = open(output, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 		
-		while ((n = read(fd[READ_END], buf, sizeof(buf))) > 0) {
-			write(fd[WRITE_END], buf, n);
+		if (dst_fd < 0) {
+			printf("An error occured opening the output file %s\n", output);
+			exit(1);
 		}
+		
+		while ((n = read(fd[READ_END], buf, sizeof(buf))) > 0) {
+			write(dst_fd, buf, n);
+		}
+
 		close(fd[READ_END]);
+		close(dst_fd);
 	} else {
 		close(fd[READ_END]);
 
 		char buf[4096];
 		ssize_t n;	
 		int src_fd = open(input, O_RDONLY);
+
+		if (src_fd < 0) {
+			printf("An error occured opening the input file. Are you sure %s exists?\n", input);
+			exit(1);
+		}
 
 		while ((n = read(src_fd, buf, sizeof(buf))) > 0) {
 			write(fd[WRITE_END], buf, n);
